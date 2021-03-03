a0d5b26d7af7e47f36b91f73562863ee1f2bad5a
Stanislav Janů <info@lweb.cz>
2021-03-03 14:22:16 +0100

    PHP 8, PhpStan, Rector.

--- src/DatabaseUpdater.php
+++ src/DatabaseUpdater.php
@@ -2,69 +2,51 @@
 
 namespace JCode;
 
-use Nette\Database\Context;
+use Nette\Utils\FileSystem;
 use Nette\Database\DriverException;
+use Nette\Database\Explorer;
 use Nette\Utils\Finder;
 use Nette\Utils\Strings;
 use Nette\Utils\DateTime;
+use Safe\Exceptions\ArrayException;
 use Tracy\Debugger;
+use function Safe\ksort;
 
-// TODO: Ajaxify
 
-/**
- * Class DatabaseUpdater
- * @package JCode
- */
 class DatabaseUpdater
 {
-	/** @var Context */
-	public $database;
+	/** @var array<int|string, array<string, mixed>> */
+	public array $index;
 
-	/** @var string */
-	public $wwwDir;
+	const DIRECTORY = '../SQL';
+	const FILE = 'index.dat';
 
-	/** @var array */
-	public $index;
 
-	const DIRECTORY = '/../SQL';
-
-	/**
-	 * DatabaseUpdater constructor.
-	 *
-	 * @param string                  $wwwDir
-	 * @param \Nette\Database\Context $database
-	 */
-	public function __construct(string $wwwDir, Context $database)
-	{
-		$this->wwwDir = $wwwDir;
-		$this->database = $database;
+	public function __construct(
+		public string $wwwDir,
+		public Explorer $database
+	) {
 	}
 
-	private function loadIndex()
+
+	private function loadIndex(): void
 	{
-		$data_file = $this->getDirectory().'/index.dat';
-		$f = fopen($data_file, 'r');
-		$serialized = fread($f, filesize($data_file));
-		fclose($f);
-		$this->index = unserialize($serialized);
+		$this->index = unserialize(FileSystem::read($this->getFilePath()));
 	}
 
-	private function saveIndex()
+
+	private function saveIndex(): void
 	{
-		$data_file = $this->getDirectory().'/index.dat';
-		$serialized = serialize($this->index);
-		$f = fopen($data_file, 'w+');
-		fwrite($f, $serialized);
-		fclose($f);
+		FileSystem::write($this->getFilePath(), serialize($this->index));
 	}
 
-	final public function run()
+
+	final public function run(): void
 	{
-		if(!Debugger::$productionMode)
-		{
+		if (!Debugger::$productionMode) {
 			$files = [];
-			foreach(Finder::findFiles('*.sql')->from($this->getDirectory()) as $item)
-			{
+			foreach (Finder::findFiles('*.sql')
+				->from($this->getDirectory()) as $item) {
 				$files[(int) pathinfo((string) $item, PATHINFO_FILENAME)] = [
 					'file' => pathinfo((string) $item, PATHINFO_BASENAME),
 					'time' => DateTime::from((int) pathinfo((string) $item, PATHINFO_FILENAME)),
@@ -72,26 +54,24 @@ class DatabaseUpdater
 				];
 			}
 
-			ksort($files);
+			try {
+				ksort($files);
+			} catch (ArrayException) {
+			}
 
-			$data_file = $this->getDirectory().'/index.dat';
-			if(file_exists($data_file))
-			{
+			if (file_exists($this->getFilePath())) {
 				$this->loadIndex();
 				$n = 0;
-				foreach($files as $loaded_file_id => $loaded_file)
-				{
-					if(!isset($this->index[$loaded_file_id]))
-					{
+				foreach ($files as $loaded_file_id => $loaded_file) {
+					if (!isset($this->index[$loaded_file_id])) {
 						$this->index[$loaded_file_id] = $loaded_file;
 						$n++;
 					}
 				}
-				if($n > 0)
+				if ($n > 0) {
 					$this->saveIndex();
-			}
-			else
-			{
+				}
+			} else {
 				$this->index = $files;
 				$this->saveIndex();
 			}
@@ -100,40 +80,32 @@ class DatabaseUpdater
 			$this->doIt();
 			$this->markAsUpdated();
 
-			$completed_files = [];
 			$new_files = [];
-			foreach($files as $loaded_file_id => $loaded_file)
-			{
-				if(isset($this->index[$loaded_file_id]))
-				{
-					if($this->index[$loaded_file_id]['completed'])
-						$completed_files[] = $this->index[$loaded_file_id];
-					else
+			foreach ($files as $loaded_file_id => $loaded_file) {
+				if (isset($this->index[$loaded_file_id])) {
+					if ($this->index[$loaded_file_id]['completed'] === null) {
 						$new_files[] = $loaded_file;
-				}
-				else
-				{
+					}
+				} else {
 					$new_files[] = $loaded_file;
 				}
 			}
 
-			if(count($new_files) > 0)
-			{
+			if (count($new_files) > 0) {
 				$f = [];
-				foreach($new_files as $file)
+				foreach ($new_files as $file) {
 					$f[] = $file['file'];
+				}
 				$all_files = implode(',', $f);
 
 				echo '<h1>New database updates:</h1>';
-				echo '<a href="?database_updater_run='.$all_files.'" style="font-size: 18px">UPDATE ALL</a>';
+				echo '<a href="?database_updater_run=' . $all_files . '" style="font-size: 18px">UPDATE ALL</a>';
 				echo '<ul>';
 				$f = [];
-				foreach($new_files as $file)
-				{
-					$fc = nl2br(Strings::truncate(Strings::normalizeNewLines(file_get_contents($this->getDirectory().'/'.$file['file'])), 512));
+				foreach ($new_files as $file) {
+					$fc = nl2br(Strings::truncate(Strings::normalizeNewLines(FileSystem::read($this->getDirectory() . '/' . $file['file'])), 512));
 					$f[] = $file['file'];
-					echo
-<<< HTML
+					echo <<< HTML
 <li>
 <h2>{$file['time']->format('j. F Y H:i')}</h2>
 <p><a href="?database_updater_run={$file['file']}">UPDATE</a>, <a href="?database_updater_mark_as_updated={$file['file']}">Mark as updated</a></p>
@@ -147,101 +119,102 @@ HTML;
 		}
 	}
 
-	private function doIt()
+
+	private function doIt(): void
 	{
-		if(!isset($_GET['database_updater_run']))
-			return null;
+		if (!isset($_GET['database_updater_run'])) {
+			return;
+		}
 
 		$hasError = false;
 
 		$files = explode(',', $_GET['database_updater_run']);
-		foreach($files as $file)
-		{
+		foreach ($files as $file) {
 			$file_id = (int) $file;
-			if(isset($this->index[$file_id]) && $this->index[$file_id]['completed'] == null)
-			{
+			if (isset($this->index[$file_id]) && $this->index[$file_id]['completed'] === null) {
 				try {
-					$sql_file = $this->getDirectory().'/'.$file;
-					$sql = file_get_contents($sql_file);
+					$sql_file = $this->getDirectory() . '/' . $file;
+					$sql = FileSystem::read($sql_file);
 					$this->database->query($sql);
 					$this->index[$file_id]['completed'] = DateTime::from('now');
 					$this->saveIndex();
-				}
-				catch (DriverException $exception)
-				{
+				} catch (DriverException $exception) {
 					$hasError = true;
-					echo '<p><strong>'.$file.'</strong> – '.$exception->getMessage().'</p>';
+					echo '<p><strong>' . $file . '</strong> – ' . $exception->getMessage() . '</p>';
 				}
-			}
-			else
-			{
+			} else {
 				$hasError = true;
-				echo '<p><strong>File '.$file.' has been not found in index file or has been marked as completed.</strong></p>';
+				echo '<p><strong>File ' . $file . ' has been not found in index file or has been marked as completed.</strong></p>';
 			}
 		}
 
-		if($hasError)
+		if ($hasError) {
 			exit;
-
+		}
 		// TODO: redirect to URL without our parameter.
 	}
 
-	private function markAsUpdated()
+
+	private function markAsUpdated(): void
 	{
-		if(!isset($_GET['database_updater_mark_as_updated']))
-			return null;
+		if (!isset($_GET['database_updater_mark_as_updated'])) {
+			return;
+		}
 
 		$files = explode(',', $_GET['database_updater_mark_as_updated']);
-		foreach($files as $file)
-		{
+		foreach ($files as $file) {
 			$file_id = (int) $file;
-			if(isset($this->index[$file_id]) && $this->index[$file_id]['completed'] == null)
-			{
+			if (isset($this->index[$file_id]) && $this->index[$file_id]['completed'] === null) {
 				$this->index[$file_id]['completed'] = DateTime::from('now');
 				$this->saveIndex();
 			}
 		}
 	}
 
-	private function doForm()
-	{
-		if(!isset($_POST['database_updater_form_new']))
-			return null;
 
-		if(!empty($_POST['database_updater_form_new_sql']))
-		{
+	private function doForm(): void
+	{
+		if (!isset($_POST['database_updater_form_new'])) {
+			return;
+		}
+
+		if (isset($_POST['database_updater_form_new_sql'])) {
 			$sql = $_POST['database_updater_form_new_sql'];
 
 			$now = DateTime::from('now');
 			$now_timestamp = $now->getTimestamp();
-			$file_name = $now_timestamp.'.sql';
+			$file_name = $now_timestamp . '.sql';
 			$this->index[$now_timestamp] = [
 				'file' => $file_name,
 				'time' => $now,
 				'completed' => $now,
 			];
 
-			$file = $this->getDirectory().'/'.$file_name;
-			$f = fopen($file, 'w+');
-			fwrite($f, $sql);
-			fclose($f);
+			FileSystem::write($this->getDirectory() . '/' . $file_name, $sql);
 
-			if(!empty($_POST['database_updater_form_new_run']))
+			if (isset($_POST['database_updater_form_new_run'])) {
 				$this->database->query($sql);
+			}
 
 			$this->saveIndex();
 
-			if(!empty($_POST['database_updater_form_back_link']))
-			{
-				header('Location: '.$_POST['database_updater_form_back_link']);
+			if (isset($_POST['database_updater_form_back_link'])) {
+				header('Location: ' . $_POST['database_updater_form_back_link']);
 				exit;
 			}
 		}
 	}
 
-	private function getDirectory() : string
+
+	private function getDirectory(): string
 	{
-		return $this->wwwDir.self::DIRECTORY;
+		return $this->wwwDir . self::DIRECTORY;
+	}
+
+
+	private function getFilePath(): string
+	{
+		return $this->wwwDir . DIRECTORY_SEPARATOR . self::DIRECTORY . DIRECTORY_SEPARATOR . self::FILE;
 	}
 
 }
--- src/panel/DatabaseUpdaterPanel.php
+++ src/panel/DatabaseUpdaterPanel.php
@@ -5,29 +5,20 @@ namespace JCode;
 use Latte\Engine;
 use Tracy\IBarPanel;
 
-/**
- * Class DatabaseUpdaterPanel
- * @package JCode
- */
+
 class DatabaseUpdaterPanel implements IBarPanel
 {
-
-	/**
-	 * @return string
-	 */
-	function getPanel() : string
+	function getPanel(): string
 	{
-		return (new Engine())->renderToString(__DIR__.'/panel.latte', [
-			'back_link' => $_SERVER['REQUEST_URI']
+		return (new Engine())->renderToString(__DIR__ . '/panel.latte', [
+			'back_link' => $_SERVER['REQUEST_URI'],
 		]);
 	}
 
-	/**
-	 * @return string
-	 */
-	function getTab() : string
+
+	function getTab(): string
 	{
-		return (new Engine())->renderToString(__DIR__.'/tab.latte');
+		return (new Engine())->renderToString(__DIR__ . '/tab.latte');
 	}
 
 }
